81ddbdc5b5743666fe7ea4baaa4e4c54adfc08f7
Dr127 <79019262+Dr127@users.noreply.github.com>
2021-06-27 11:31:25 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,10 +1,13 @@
 function send(){
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
-    actual_answer = parseInt(number1) * parseInt(number2);
+    console.log(number1);
+    console.log(number2)
+   actual_answer = parseInt(number1) * parseInt(number2);
+   console.log(actual_answer);
     question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
-    input_box = "<br>Answer : <input type = 'text' id ='input_check_box'>";
-    check_button = "<br><br><button class = 'btn-btn-infor' onclick = 'check'>Check</button>";
+    input_box = "<br><br>Answer : <input type = 'text' id ='input_check_box'>";
+    check_button = "<br><br><button class = 'btn-btn-info' style='width: 100px, height: 50px;' onclick = 'check'>Check</button>";
     row = question_number + input_box + check_button;
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
